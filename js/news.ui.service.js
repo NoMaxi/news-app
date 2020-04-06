298538e09d3b50b220406ba7aad4361358432e4f
NoMaxi <max_novik@ukr.net>
2020-04-06 19:41:27 +0300

    improve news article card template

--- js/news.ui.service.js
+++ js/news.ui.service.js
@@ -4,89 +4,44 @@ class NewsUI {
   }
 
   /**
-   *
+   * generate an article template
    * @param {Object} article
+   * @returns {string}
    */
   static generateArticleTemplate(article) {
-    // searching for articles by keyword 'helicopter' takes 4.1-5.7 ms
-    // return `
-    // <div class="col s12 m6">
-    //     <div class="card">
-    //         <div class="card-image">
-    //             <img src="${article.urlToImage}">
-    //         </div>
-    //         <div class="card-content">
-    //             <span class="card-title">${article.title || ''}</span>
-    //             <p>${article.description || ''}</p>
-    //         </div>
-    //         <div class="card-action">
-    //             <a href="${article.url}" target="_blank">Read more</a>
-    //         </div>
-    //     </div>
-    // </div>
-    // `;
-
-    // searching for articles by keyword 'helicopter' takes 3.8-4.2 ms
-    const articleContainer = document.createElement('div');
-    articleContainer.classList.add('col', 's12', 'm6');
-
-    const card = document.createElement('div');
-    card.classList.add('card');
-
-    articleContainer.appendChild(card);
-
-    const cardImage = document.createElement('div');
-    cardImage.classList.add('card-image');
-
-    const img = document.createElement('img');
-    img.src = `${article.urlToImage}`;
-
-    cardImage.appendChild(img);
-
-    const cardContent = document.createElement('div');
-    cardContent.classList.add('card-content');
-
-    const cardTitle = document.createElement('span');
-    cardTitle.classList.add('card-title');
-    cardTitle.textContent = `${article.title || ''}`;
-
-    const description = document.createElement('p');
-    description.textContent = `${article.description || ''}`;
-
-    cardContent.appendChild(cardTitle);
-    cardContent.appendChild(description);
-
-    const cardAction = document.createElement('div');
-    cardAction.classList.add('card-action');
-
-    const link = document.createElement('a');
-    link.href = `${article.url}`;
-    link.target = '_blank';
-    link.textContent = 'Read more';
-
-    cardAction.appendChild(link);
-
-    card.appendChild(cardImage);
-    card.appendChild(cardContent);
-    card.appendChild(cardAction);
-
-    return articleContainer;
+    return `
+      <div class="col s12 m6">
+        <div class="card">
+          <div class="card-image">
+            <img src="${ article.urlToImage }" alt="article-img">
+          </div>
+          <div class="card-content">
+            <span class="card-title">
+              <a href="${ article.url }" target="_blank">${ article.title || '' }</a>
+            </span>
+            <p class="card-date">
+              ${ new Date(article.publishedAt).toLocaleString() || '' }
+            </p>
+            <p>${ article.description || '' }</p>
+          </div>
+          <div class="card-action">
+            <a href="${ article.url }" target="_blank">
+              <span>Read more</span>
+              <span class="card-source-name">${ article.source.name || '' }</span>
+            </a>
+          </div>
+        </div>
+      </div>
+    `;
   }
 
   /**
-   *
+   * add article to DOM
    * @param {Object} article
    */
   addArticle(article) {
     const template = NewsUI.generateArticleTemplate(article);
-
-    // console.time();
-    // this.newsContainer.insertAdjacentHTML('afterbegin', template);
-    // console.timeEnd();
-
-    // console.time();
-    this.newsContainer.appendChild(template);
-    // console.timeEnd();
+    this.newsContainer.insertAdjacentHTML('afterbegin', template);
   }
 
   clearContainer() {
